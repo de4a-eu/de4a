9b48739c161269d9406f239c1d3f8978b584d40f
pablo.peique <pablo.peique@externos.correo.gob.es>
2021-02-04 10:30:16 +0100

    changes in xsds interfaces

--- de4a-connector/src/main/java/eu/toop/as4/domibus/soap/auto/package-info.java
+++ de4a-connector/src/main/java/eu/toop/as4/domibus/soap/auto/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.02.01 a las 02:37:52 PM CET 
+// Generado el: 2021.02.04 a las 09:31:57 AM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
