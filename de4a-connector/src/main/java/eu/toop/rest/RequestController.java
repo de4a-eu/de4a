6fa3d47cc6af0ce663370e2c8fc8225e406234ed
pablo.peique <pablo.peique@externos.correo.gob.es>
2021-02-10 10:16:53 +0100

    Inicio del control de errores del servicio REST del connector

--- de4a-connector/src/main/java/eu/toop/rest/RequestController.java
+++ de4a-connector/src/main/java/eu/toop/rest/RequestController.java
@@ -1,10 +1,12 @@
 package eu.toop.rest;
-
+ 
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,30 +17,39 @@ import eu.toop.req.model.EvaluatorRequest;
 import eu.toop.req.repository.EvaluatorRequestRepository;
 import eu.toop.service.EvidenceRequestorManager;
 @Controller 
+@Validated
 public class RequestController {
 	private static final Logger logger =  LoggerFactory.getLogger (RequestController.class);
 	@Autowired
 	private EvaluatorRequestRepository   evaluatorRequestRepository;
 	@Autowired
-	private EvidenceRequestorManager evidenceRequestorManager; 
-	/*public @ResponseBody Ack sendRequest(@RequestParam  String urlReturn,@RequestParam String evaluatorId,@RequestParam String requestId) 
-	{ 
-		EvaluatorRequest entity=new EvaluatorRequest();
-		entity.setIdevaluator(evaluatorId);
-		entity.setIdrequest(requestId);
-		entity.setUrlreturn(urlReturn);
-		evaluatorRequestRepository.save(entity);
-		logger.debug("Saving evaluator request evaluator:{},request:{},urlreturn:{}",evaluatorId,requestId, urlReturn);
-		boolean ok=evidenceRequestorManager.yourfather(requestId,"");
-		Ack ack=new Ack();
-		ack.setCode(ok?"OK":"FAIL");
-		ack.setMessage(ok?"In process":"Repeat again"); 
-		return ack;
-	}*/
-	//@RequestMapping(method = RequestMethod.POST, value="/request", headers="Accept=*/*",   consumes = {   "application/xml" },  produces = {  "application/xml" }   )
+	private EvidenceRequestorManager evidenceRequestorManager;  
 	@PostMapping(value = "/request", consumes = {   "application/xml" },  produces = {  "application/xml" }  )
-	public @ResponseBody Ack sendRequest(@RequestBody RequestTransferEvidence request) 
-	{ 
+	public @ResponseBody Ack sendRequest(@Valid @RequestBody RequestTransferEvidence request) 
+{  
+		//TODO Inclusión de validación del mensaje de entrada contra el xsd 
+		
+
+		
+//JAXBSource source=null;
+//	try {
+//		source = new JAXBSource(JAXBContext.newInstance(RequestTransferEvidence.class), request);
+//	} catch (JAXBException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//	
+//	Schema schema;
+//	try {
+//		SchemaFactory sf = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
+//		schema =  sf.newSchema(new StreamSource(this.getClass().getResourceAsStream(  "requestor/DR-IM.xsd" )));
+//		Validator validator = schema.newValidator();
+//		validator.setErrorHandler(new CustomValidationErrorHandler());
+//		validator.validate(source);
+//	} catch ( Exception e) {
+//		 
+//	}
+	
 		EvaluatorRequest entity=new EvaluatorRequest();
 		entity.setIdevaluator(request.getDataEvaluator().getId());
 		entity.setIdrequest(request.getRequestId());
