bc7163f0a41784cfebd508b9ed76dfde8e2c4831
pablo.peique <pablo.peique@externos.correo.gob.es>
2021-02-11 14:05:31 +0100

    Actualizacion xsd requestor

--- de4a-commons/src/main/java/eu/de4a/exception/MessageException.java
+++ de4a-commons/src/main/java/eu/de4a/exception/MessageException.java
@@ -2,7 +2,29 @@ package eu.de4a.exception;
 
 public class MessageException extends Throwable{ 
 	private static final long serialVersionUID = 1L;
+	private String code;
+	private String message;
 	public MessageException(String err) {
 		super(err);
+		this.message=err;
+		this.code=ExceptionMapping.UNDEFINED.getCode();
 	}
+	public MessageException(ExceptionMapping exmapping) {
+		super(exmapping.getMessage());
+		this.message=exmapping.getMessage();
+		this.code=exmapping.getCode();
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
 }
