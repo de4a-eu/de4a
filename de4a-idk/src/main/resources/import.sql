65acdc6ce96ab6a74133b0722dc2f59ba79bbef7
pablo.peique <pablo.peique@externos.correo.gob.es>
2021-02-11 14:08:57 +0100

    Inclusión de DBA evidence

--- de4a-idk/src/main/resources/import.sql
+++ de4a-idk/src/main/resources/import.sql
@@ -1,8 +1,10 @@
 
-
 INSERT INTO IssuingAuthority (id, evidenceType, countryCode, iaLevelPath) VALUES (1, 'HigherEdCertificate','PT', 'nuts0/edu');
 INSERT INTO IssuingAuthority (id, evidenceType, countryCode, iaLevelPath) VALUES (2, 'BirthCertificate','ES', 'nuts0/soc');
+INSERT INTO IssuingAuthority (id, evidenceType, countryCode, iaLevelPath) VALUES (3, 'DoingBusinessAbroad','ES', 'nuts0/soc');
 INSERT INTO AtuItem (id, id_issuingAuthority, atuLevel, atuPath, atuCode, atuName, atuLatinName) VALUES (1, 1, 'nuts0', 'nuts0/edu', 'ptUniversityOfLisbon', 'Universidad de Lisboa', 'Universidad de Lisboa');
 INSERT INTO AtuItem (id, id_issuingAuthority, atuLevel, atuPath, atuCode, atuName, atuLatinName) VALUES (2, 2, 'nuts0', 'nuts0/soc', 'MPTFP-SGAD', 'Registro Civil', 'Registro Civil');
+INSERT INTO AtuItem (id, id_issuingAuthority, atuLevel, atuPath, atuCode, atuName, atuLatinName) VALUES (3, 3, 'nuts0', 'nuts0/soc', 'MPTFP-SGAD', 'DBA_MOCK', 'DBA_MOCK');
 INSERT INTO EvidenceService (id, countryCode, atuCode, canonicalEvidence, service, dataOwner, dataTransferor, redirectURL) VALUES (1, 'PT', 'ptUniversityOfLisbon', 'HigherEdCertificate', 'urn::de4a.eu:identifiers:service::9991:PT000000029:HighEdEdCertificate:1.0', 'iso6523-actorid-upis::9991:PT000000101', 'iso6523-actorid-upis::9991:PT000000029', 'https://fenix-edu-inesc.pt/usip');
 INSERT INTO EvidenceService (id, countryCode, atuCode, canonicalEvidence, service, dataOwner, dataTransferor, redirectURL) VALUES (2, 'ES', 'MPTFP-SGAD', 'BirthCertificate', 'urn::de4a.eu:identifiers:service::9921:S2833002E:BirthCertificate:1.0', '9915:tooptest', '9915:tooptest', 'https://intermediacionpp.redsara.es/servicios/SVD/RegistroCivil.ConsultaNacimiento');
+INSERT INTO EvidenceService (id, countryCode, atuCode, canonicalEvidence, service, dataOwner, dataTransferor, redirectURL) VALUES (3, 'ES', 'MPTFP-SGAD', 'DoingBusinessAbroad', 'urn::de4a.eu:identifiers:service::9921:S2833002E:DoingBusinessAbroad:1.0', '9915:tooptest', '9915:tooptest', 'https://intermediacionpp.redsara.es/servicios/SVD/RegistroCivil.ConsultaNacimiento');
