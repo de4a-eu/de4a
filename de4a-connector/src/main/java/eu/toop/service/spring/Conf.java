85e5d950866527b4eba214f46c48b23111508596
Antonio Osuna <aosunacab@yahoo.com>
2021-03-10 15:09:52 +0100

    Fix configuration Swagger UI

--- de4a-connector/src/main/java/eu/toop/service/spring/Conf.java
+++ de4a-connector/src/main/java/eu/toop/service/spring/Conf.java
@@ -110,8 +110,10 @@ public class Conf implements WebMvcConfigurer {
 	
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.
+            addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
     }
 	
 	@Bean(initMethod = "start", destroyMethod = "stop")
