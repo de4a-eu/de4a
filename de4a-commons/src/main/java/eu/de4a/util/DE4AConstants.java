2c4c00b2abeba9a0696e330183e8ba9c2ef39f7a
Antonio Osuna <aosunacab@yahoo.com>
2021-04-09 13:57:57 +0200

    Reorganize dependencies Remove useless RestUtils class Refactor XDE4AMarshaller

--- de4a-commons/src/main/java/eu/de4a/util/DE4AConstants.java
+++ de4a-commons/src/main/java/eu/de4a/util/DE4AConstants.java
@@ -1,14 +1,5 @@
 package eu.de4a.util;
 
-
-import com.helger.commons.collection.impl.CommonsArrayList;
-import com.helger.commons.collection.impl.ICommonsList;
-import com.helger.commons.io.resource.ClassPathResource;
-import com.helger.xsds.xml.CXML_XSD;
-
-import eu.de4a.conn.api.canonical.ObjectFactory;
-import eu.de4a.iem.xml.de4a.CDE4AJAXB;
-
 public class DE4AConstants {
 	public static final String ID_MIMEPART="id";
 
@@ -70,33 +61,4 @@ public class DE4AConstants {
 	private DE4AConstants() {
 		//empty private constructor
 	}
-	
-	public static final ClassPathResource XSD_BIRTH_CERTIFICATE = new ClassPathResource ("xsds/canonicalEvidences/BirthCertificate.xsd", 
-			DE4AConstants.class.getClassLoader());
-	
-	public static ICommonsList <ClassPathResource> getBirthCertificateXSDs ()
-	  {
-	    final ICommonsList <ClassPathResource> a = new CommonsArrayList <> ();
-	    a.add (CXML_XSD.getXSDResource ());
-	    a.addAll (CDE4AJAXB.XSDS);
-	    a.add (XSD_BIRTH_CERTIFICATE);
-	    return a;
-	  }
-	
-	public static final Class <?> [] aClasses = new Class <?> [] { com.helger.xsds.ccts.cct.schemamodule.ObjectFactory.class,
-        com.helger.xsds.xlink.ObjectFactory.class,
-        eu.de4a.iem.jaxb.common.idtypes.ObjectFactory.class,
-        eu.de4a.iem.jaxb.common.types.ObjectFactory.class,
-        eu.de4a.iem.jaxb.cv.dt.ObjectFactory.class,
-        eu.de4a.iem.jaxb.de_usi.ObjectFactory.class,
-        eu.de4a.iem.jaxb.do_im.ObjectFactory.class,
-        eu.de4a.iem.jaxb.do_usi.ObjectFactory.class,
-        eu.de4a.iem.jaxb.dr_im.ObjectFactory.class,
-        eu.de4a.iem.jaxb.dr_usi.ObjectFactory.class,
-        eu.de4a.iem.jaxb.do_usi.ObjectFactory.class,
-        eu.de4a.iem.jaxb.eidas.lp.ObjectFactory.class,
-        eu.de4a.iem.jaxb.eidas.np.ObjectFactory.class,
-        eu.de4a.iem.jaxb.idk.ObjectFactory.class,
-        eu.de4a.iem.jaxb.w3.cv.bc.ObjectFactory.class,
-        ObjectFactory.class};
 }
