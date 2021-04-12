b6630dd9ba3e79678c907bda1b6cdd8c48e23154
Antonio Osuna <aosunacab@yahoo.com>
2021-04-12 13:41:55 +0200

    Fix Sonar bugs

--- de4a-connector/src/main/java/eu/de4a/connector/api/manager/EvidenceRequestorManager.java
+++ de4a-connector/src/main/java/eu/de4a/connector/api/manager/EvidenceRequestorManager.java
@@ -106,6 +106,7 @@ public class EvidenceRequestorManager extends EvidenceManager {
 
 		} catch (InterruptedException e) {
 			logger.error("Error waiting for response", e);
+			Thread.currentThread().interrupt();
 			return null;
 		}
 		if (!ok) {
