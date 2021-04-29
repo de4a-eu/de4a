fafe5b08945bda4da42c30a59d3fe0d0b51c1fe4
Antonio Osuna <aosunacab@yahoo.com>
2021-04-29 15:03:53 +0200

    Error handling sending RequestTransferEvidence to the Owner

---
+++ de4a-connector/src/main/java/eu/de4a/connector/api/controller/error/ErrorHandlerUtils.java
@@ -0,0 +1,71 @@
+package eu.de4a.connector.api.controller.error;
+
+import java.text.MessageFormat;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import eu.de4a.iem.xml.de4a.DE4AMarshaller;
+
+public class ErrorHandlerUtils {
+    
+    ErrorHandlerUtils() {
+        //empty constructor
+    }
+    
+    public static boolean checkResponse(ResponseEntity<String> response, ExternalModuleError module, 
+            ConnectorException ex) {
+        if(response == null || !HttpStatus.ACCEPTED.equals(response.getStatusCode()) 
+                || ObjectUtils.isEmpty(response.getBody())) {
+            throw ex.withLayer(LayerError.COMMUNICATIONS)
+                .withFamily(FamilyErrorType.ERROR_RESPONSE) 
+                .withModule(module)
+                .withMessageArg(MessageFormat.format("Failed or empty response received {0}", response))
+                .withHttpStatus(HttpStatus.OK);
+        }
+        return true;
+    }
+    
+    public static String postRestObjectWithCatching(String url, String request, ExternalModuleError module, 
+            ConnectorException ex, RestTemplate restTemplate) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+        ResponseEntity<String> response;
+        try {
+            response = restTemplate.postForEntity(url,
+                    new HttpEntity<>(request, headers), String.class);
+        } catch(RestClientException e) { 
+            throw ex.withLayer(LayerError.COMMUNICATIONS)
+            .withFamily(FamilyErrorType.CONNECTION_ERROR) 
+            .withModule(module)
+            .withMessageArg(e.getMessage())
+            .withHttpStatus(HttpStatus.OK);
+        }
+        checkResponse(response, module, ex);
+        return response.getBody();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> Object conversionWithCatching(DE4AMarshaller<T> marshaller, Object obj, boolean objToStr, 
+            LayerError layer, ExternalModuleError module, ConnectorException ex) {
+        try {
+            if(objToStr) {
+                return marshaller.getAsString((T) obj);
+            }
+            return marshaller.read((String) obj);
+        } catch(Exception e) { 
+            throw ex.withLayer(layer)
+            .withFamily(FamilyErrorType.CONVERSION_ERROR) 
+            .withModule(module)
+            .withMessageArg(e.getMessage())
+            .withHttpStatus(HttpStatus.OK);
+        }
+    }
+
+}
---
+++ de4a-connector/src/main/java/eu/de4a/connector/api/controller/error/ResponseErrorExceptionHandler.java
@@ -0,0 +1,20 @@
+package eu.de4a.connector.api.controller.error;
+
+import eu.de4a.iem.jaxb.common.types.ErrorListType;
+import eu.de4a.iem.jaxb.common.types.ResponseErrorType;
+import eu.de4a.iem.xml.de4a.DE4AMarshaller;
+import eu.de4a.iem.xml.de4a.DE4AResponseDocumentHelper;
+
+public class ResponseErrorExceptionHandler extends ConnectorExceptionHandler {
+
+    @Override
+    public String getResponseError(ConnectorException ex) {
+        ResponseErrorType response = DE4AResponseDocumentHelper.createResponseError(false);
+        ErrorListType errorList = new ErrorListType();
+        String msg = getMessage(ex);
+        errorList.addError(DE4AResponseDocumentHelper.createError(ex.buildCode(), msg));
+        response.setErrorList(errorList);
+        return DE4AMarshaller.doUsiResponseMarshaller().getAsString(response);
+    }
+
+}
