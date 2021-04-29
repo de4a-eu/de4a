6e1564a1e34aafdf3728bb96c4fa7da290469f6f
pablo.peique <pablo.peique@externos.correo.gob.es>
2021-04-29 15:08:18 +0200

    Handling owners exceptions

--- de4a-connector/src/main/java/eu/de4a/connector/as4/owner/OwnerLocator.java
+++ de4a-connector/src/main/java/eu/de4a/connector/as4/owner/OwnerLocator.java
@@ -5,19 +5,23 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import eu.de4a.connector.api.controller.error.ExternalModuleError;
+import eu.de4a.connector.api.controller.error.FamilyErrorType;
+import eu.de4a.connector.api.controller.error.LayerError;
+import eu.de4a.connector.api.controller.error.OwnerException;
 import eu.de4a.connector.model.OwnerAddresses;
 import eu.de4a.connector.repository.OwnerAddressesRepository;
-import eu.de4a.exception.MessageException;
 
 @Component
 public class OwnerLocator {
 	@Autowired
 	private OwnerAddressesRepository evidenceEntityRepository;
 
-	public OwnerAddresses lookupOwnerAddress(String dataOwnerId) throws MessageException {
+	public OwnerAddresses lookupOwnerAddress(String dataOwnerId) throws OwnerException {
 		OwnerAddresses evidence = evidenceEntityRepository.findById(dataOwnerId.toLowerCase(Locale.ROOT)).orElse(null);
 		if (evidence == null) {
-			throw new MessageException(String.format("Does not exists information for Owner id {}", dataOwnerId));
+			throw new OwnerException().withFamily(FamilyErrorType.CONNECTION_ERROR).withLayer(LayerError.CONFIGURATION).withMessage("error.owner.not.found").
+			withMessageArg(dataOwnerId).withModule(ExternalModuleError.DATA_OWNER);
 		}
 		return evidence;
 	}
