1a7655f73b0fcf3449127c9fa7b5d92e6e0695c0
pablo.peique <pablo.peique@externos.correo.gob.es>
2021-04-29 09:39:11 +0200

    Connector error handling

---
+++ de4a-connector/src/main/java/eu/de4a/connector/api/controller/error/DetailMessageError.java
@@ -0,0 +1,28 @@
+package eu.de4a.connector.api.controller.error;
+
+public enum DetailMessageError { 
+	RESOURCE_UNAVAILABLE("01","error.resource.unavailable"), 
+	SCHEMA_FAILED("02","error.schema.failed"),
+	MISSING_REQUIRED_ARGUMENTS("03","error.missing.arguments") ,
+	EMPTY_RESPONSE("05","error.empty.response"),
+	CONNECTION_TIMEOUT("06","error.timeout"),
+	UNABLE_RESOLVE_HOST("07","error.unable.resolve.host"),
+	SSL_HANDSHAKE_FAIL("08","error.ssl.handshake"),
+	SMP_DATA_NOT_FOUND("09","error.smp.data.not.found"), 
+	TRANSFORMATION_MESSAGE_ERR ("23","error.message.transformation"), 
+	CONFIGURATION ("25","error.configuration"), 
+	REGREP_ERROR("26","error.regrep.building");
+	private String id;
+	private String label;
+	DetailMessageError(String id,String label){
+	    this.id = id;
+	    this.label=label;
+	}
+	public String getID(){
+	    return id;
+	}
+	public String getLabel() {
+		return label;
+	}
+ 
+}
---
+++ de4a-connector/src/main/java/eu/de4a/connector/api/controller/error/FamilyErrorType.java
@@ -0,0 +1,23 @@
+package eu.de4a.connector.api.controller.error;
+
+public enum FamilyErrorType { 
+	MISSING_REQUIRED_ARGUMENTS("2","error.missing.arguments"),
+	CONNECTION_ERROR("3","error.connection") ,
+	ERROR_RESPONSE("4",""),
+	CONVERSION_ERROR("5",""),
+	SAVING_DATA_ERROR("6",""),
+	SCHEMA_VALIDATION_FAILED("7",""),
+	AS4_ERROR_COMMUNICATION("8","") ;
+	private String id;
+	private String label;
+	FamilyErrorType(String id,String label){
+	    this.id = id;
+	    this.label=label;
+	}
+	public String getID(){
+	    return id;
+	}
+	public String getLabel() {
+		return label;
+	} 
+}
