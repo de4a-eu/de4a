4443b74b87fddd190a86bae28d7add0503fa6b5e
Antonio Osuna <aosunacab@yahoo.com>
2021-05-04 12:00:35 +0200

    Error handling - Include message keys and utils

--- de4a-connector/src/main/java/eu/de4a/connector/api/controller/CustomRestExceptionHandler.java
+++ de4a-connector/src/main/java/eu/de4a/connector/api/controller/CustomRestExceptionHandler.java
@@ -7,9 +7,6 @@ import javax.xml.bind.UnmarshalException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -28,77 +25,88 @@ import eu.de4a.connector.error.exceptions.ConnectorException;
 import eu.de4a.connector.error.model.ExternalModuleError;
 import eu.de4a.connector.error.model.FamilyErrorType;
 import eu.de4a.connector.error.model.LayerError;
+import eu.de4a.connector.error.model.MessageKeys;
 import eu.de4a.connector.error.utils.ResponseErrorFactory;
+import eu.de4a.connector.service.spring.MessageUtils;
 import eu.de4a.iem.jaxb.common.types.ErrorListType;
 import eu.de4a.iem.jaxb.common.types.ResponseErrorType;
 import eu.de4a.iem.xml.de4a.DE4AResponseDocumentHelper;
+
 /**
  * Controller for handling BAD_REQUEST type errors for more concise messages
  *
  */
 @ControllerAdvice
 public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
-	private static final Logger LOG = LoggerFactory.getLogger(CustomRestExceptionHandler.class);
-	@Autowired
-	private MessageSource messageSource;
-	@Override
-	protected ResponseEntity<Object> handleMissingServletRequestParameter(
-	  MissingServletRequestParameterException ex, HttpHeaders headers,
-	  HttpStatus status, WebRequest request) {
-	  String error = ex.getParameterName() + " parameter is missing";
-	  return buildBadRequestError(error);
-	}
-	@ExceptionHandler(value = { ConnectorException.class })
-	protected ResponseEntity<Object> handleConnectorException(ConnectorException ex, WebRequest request) {  
-		return new ResponseEntity<>(ResponseErrorFactory.getResponseError(ex), new HttpHeaders(), ex.getStatus());
-	}
-	@ExceptionHandler(value = { Exception.class })
-	protected ResponseEntity<Object> handleExceptionUnknown(Exception ex, WebRequest request) {  
-		return new ResponseEntity<>(ResponseErrorFactory.getGenericResponseError(ex), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-	@Override
-	protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
-			HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) { 
-		List<MediaType> mediaTypes = ex.getSupportedMediaTypes();
-		if (!CollectionUtils.isEmpty(mediaTypes)) {
-			headers.setAccept(mediaTypes);
-		}
-		List<String> args = new ArrayList<>();
-		args.add(String.valueOf(ex.getContentType()));
-		args.add(mediaTypes.toString());
-		String err= messageSource.getMessage("error.400.mimetype", args.toArray(),LocaleContextHolder.getLocale()); 
-		return buildBadRequestError(err);
-	}
-	@Override
-	protected ResponseEntity<Object> handleHttpMessageNotReadable( HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-		  String error;
-		  if(ex.getCause() instanceof UnmarshalException) {
-			  String args[]= {ex.getMessage()};
-			  error= messageSource.getMessage("error.400.args.unmarshalling", args,LocaleContextHolder.getLocale());
-		  }else {
-			  error=messageSource.getMessage("error.400.args.required", null,LocaleContextHolder.getLocale());
-		  }
-		  return buildBadRequestError(error); 
-	}
-	@Override
-	protected ResponseEntity<Object> handleNoHandlerFoundException(
-			NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-			logger.warn("REST Client request not found service");
-		 	String err= messageSource.getMessage("error.404", null,LocaleContextHolder.getLocale());  
-			ResponseErrorType responseError=DE4AResponseDocumentHelper.createResponseError(false);
-			responseError.setErrorList(new ErrorListType()); 
-			//TODO same code for not found vs missing parameters?
-			String code= LayerError.COMMUNICATIONS.ordinal() + ExternalModuleError.NONE.getId() + FamilyErrorType.MISSING_REQUIRED_ARGUMENTS.getID() ;
-			responseError.getErrorList().addError(DE4AResponseDocumentHelper.createError(code, err));  
-			return new ResponseEntity<>(responseError, new HttpHeaders(), HttpStatus.NOT_FOUND);
-			
-	}
-	private ResponseEntity<Object> buildBadRequestError(String err) {
-		LOG.warn("REST Client BAD REQUEST-> {}",err);
-		ResponseErrorType responseError=DE4AResponseDocumentHelper.createResponseError(false);
-		responseError.setErrorList(new ErrorListType()); 
-		String code= LayerError.COMMUNICATIONS.ordinal() + ExternalModuleError.NONE.getId() + FamilyErrorType.MISSING_REQUIRED_ARGUMENTS.getID() ;
-		responseError.getErrorList().addError(DE4AResponseDocumentHelper.createError(code, err));  
-		return new ResponseEntity<>(responseError, new HttpHeaders(), HttpStatus.BAD_REQUEST);
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(CustomRestExceptionHandler.class);
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = ex.getParameterName() + " parameter is missing";
+        return buildBadRequestError(error);
+    }
+
+    @ExceptionHandler(value = { ConnectorException.class })
+    protected ResponseEntity<Object> handleConnectorException(ConnectorException ex, WebRequest request) {
+        return new ResponseEntity<>(ResponseErrorFactory.getResponseError(ex), new HttpHeaders(), ex.getStatus());
+    }
+
+    @ExceptionHandler(value = { Exception.class })
+    protected ResponseEntity<Object> handleExceptionUnknown(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ResponseErrorFactory.getGenericResponseError(ex), new HttpHeaders(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<MediaType> mediaTypes = ex.getSupportedMediaTypes();
+        if (!CollectionUtils.isEmpty(mediaTypes)) {
+            headers.setAccept(mediaTypes);
+        }
+        List<String> args = new ArrayList<>();
+        args.add(String.valueOf(ex.getContentType()));
+        args.add(mediaTypes.toString());
+        String err = new MessageUtils(MessageKeys.ERROR_400_MIMETYPE, args.toArray()).value();
+        return buildBadRequestError(err);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error;
+        if (ex.getCause() instanceof UnmarshalException) {
+            String args[] = { ex.getMessage() };
+            error = new MessageUtils(MessageKeys.ERROR_400_UNMARSHALLING, args).value();
+        } else {
+            error = new MessageUtils(MessageKeys.ERROR_400_ARGS_REQUIRED, null).value();
+        }
+        return buildBadRequestError(error);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        logger.warn("REST Client request not found service");
+        String err = new MessageUtils(MessageKeys.ERROR_404, null).value();
+        ResponseErrorType responseError = DE4AResponseDocumentHelper.createResponseError(false);
+        responseError.setErrorList(new ErrorListType());
+        // TODO same code for not found vs missing parameters?
+        String code = LayerError.COMMUNICATIONS.ordinal() + ExternalModuleError.NONE.getId()
+                + FamilyErrorType.MISSING_REQUIRED_ARGUMENTS.getID();
+        responseError.getErrorList().addError(DE4AResponseDocumentHelper.createError(code, err));
+        return new ResponseEntity<>(responseError, new HttpHeaders(), HttpStatus.NOT_FOUND);
+
+    }
+
+    private ResponseEntity<Object> buildBadRequestError(String err) {
+        LOG.warn("REST Client BAD REQUEST-> {}", err);
+        ResponseErrorType responseError = DE4AResponseDocumentHelper.createResponseError(false);
+        responseError.setErrorList(new ErrorListType());
+        String code = LayerError.COMMUNICATIONS.ordinal() + ExternalModuleError.NONE.getId()
+                + FamilyErrorType.MISSING_REQUIRED_ARGUMENTS.getID();
+        responseError.getErrorList().addError(DE4AResponseDocumentHelper.createError(code, err));
+        return new ResponseEntity<>(responseError, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
 }
