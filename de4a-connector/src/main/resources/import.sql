fb3d3e1d2e448d6db1bbbc5ee6cac7636ecc2d10
Antonio Osuna <aosunacab@yahoo.com>
2021-09-24 14:53:49 +0200

    Real Participants owner/evaluator addresses

--- de4a-connector/src/main/resources/import.sql
+++ de4a-connector/src/main/resources/import.sql
@@ -8,7 +8,16 @@ INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::
 INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:si000000016', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
 INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:pt990000101', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
 INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:pt000000026', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
-INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9921:ess2833002e', 'http://localhost:8782/de4a-pid-owner/');
+
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9920:ess2833002e', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:at000000271', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:nl990000106', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:se000000013', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:ro000000006', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:nl000000024', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:si000000016', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:pt990000101', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
+INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:pt000000026', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
 
 /* DEMO DataEvaluator addresses info */
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:ess2833002e', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
@@ -18,6 +27,19 @@ INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-up
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:ro000000006', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:nl000000024', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:si000000016', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:si000000018', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:pt990000101', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:esq6250003h', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:lu000000025', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9920:ess2833002e', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:at000000271', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:nl990000106', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:se000000013', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:ro000000006', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:nl000000024', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:si000000016', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:si000000018', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:pt990000101', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9920:esq6250003h', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:lu000000025', 'https://de4a-dev-mock.egovlab.eu/de1/usi/');
