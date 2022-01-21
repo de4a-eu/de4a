ad13748ba2532947db19ef1f3924bd61a8ec7d78
aosunacab <78353490+aosunacab@users.noreply.github.com>
2022-01-21 12:22:49 +0100

    Update import.sql with the new playground mock DE

--- de4a-connector/src/main/resources/import.sql
+++ de4a-connector/src/main/resources/import.sql
@@ -20,26 +20,26 @@ INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::
 INSERT INTO owner_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:pt000000026', 'https://pre-smp-dr-de4a.redsara.es/de4a-mock-connector/');
 
 /* DEMO DataEvaluator addresses info */
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:ess2833002e', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:at000000271', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:nl990000106', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:se000000013', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:ro000000006', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:nl000000024', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:si000000016', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:si000000018', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:pt990000101', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:esq6250003h', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:lu000000025', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:ess2833002e', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:at000000271', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:nl990000106', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:se000000013', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:ro000000006', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:nl000000024', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:si000000016', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:si000000018', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:pt990000101', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:esq6250003h', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9999:lu000000025', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
 
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9920:ess2833002e', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:at000000271', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:nl990000106', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:se000000013', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:ro000000006', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:nl000000024', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:si000000016', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:si000000018', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9920:ess2833002e', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:at000000271', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:nl990000106', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:se000000013', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:ro000000006', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:nl000000024', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:si000000016', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:si000000018', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:pt990000101', 'https://de4a-portal.gsd.inesc-id.pt:8443/api/de/');
 INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9920:esq6250003h', 'https://ujiapps.uji.es/sdg/rest/services/');
-INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:lu000000025', 'https://de4a.simplegob.com/demoui-it1/de1/usi/');
+INSERT INTO evaluator_addresses (agentUrn, endpoint) VALUES ('iso6523-actorid-upis::9991:lu000000025', 'https://de4a-demoui.informatika.uni-mb.si/de1/usi/');
