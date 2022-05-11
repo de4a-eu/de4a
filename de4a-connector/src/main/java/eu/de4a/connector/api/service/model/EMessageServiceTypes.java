39f91179582cafc734cb2cae23e36908b5ecc75f
Philip Helger <philip@helger.com>
2022-05-11 20:13:44 +0200

    Ctd

--- de4a-connector/src/main/java/eu/de4a/connector/api/service/model/EMessageServiceTypes.java
+++ de4a-connector/src/main/java/eu/de4a/connector/api/service/model/EMessageServiceTypes.java
@@ -3,59 +3,67 @@ package eu.de4a.connector.api.service.model;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+
 import com.helger.commons.annotation.Nonempty;
 
 /**
- * Enum for controlling correspondences between message types (root XML element name) and DE/DO
- * addresses properties {@link application.yml}. If something changes on {@link eu.de4a.iem.core} it
- * forces to update this class and keep it up to date
- *
+ * Enum for controlling correspondences between message types (root XML element
+ * name) and DE/DO addresses properties. If something changes on
+ * {@link eu.de4a.iem.core} it forces to update this class and keep it up to
+ * date
  */
-public enum EMessageServiceTypes {
-  IM(eu.de4a.iem.core.jaxb.dr.ObjectFactory._RequestTransferEvidenceIM_QNAME.getLocalPart(), true),
-  USI(eu.de4a.iem.core.jaxb.dr.ObjectFactory._RequestTransferEvidenceUSI_QNAME.getLocalPart(), true),
-  LU(eu.de4a.iem.core.jaxb.dr.ObjectFactory._RequestTransferEvidenceLU_QNAME.getLocalPart(), true),
-  SN(eu.de4a.iem.core.jaxb.dr.ObjectFactory._RequestEventSubscription_QNAME.getLocalPart(), true),
-  RESPONSE(eu.de4a.iem.core.jaxb.dt.ObjectFactory._ResponseTransferEvidence_QNAME.getLocalPart(), false),
-  SUBSCRIPTION_RESP(eu.de4a.iem.core.jaxb.dt.ObjectFactory._ResponseEventSubscription_QNAME.getLocalPart(), false),
-  REDIRECT(eu.de4a.iem.core.jaxb.dt.ObjectFactory._USIRedirectUser_QNAME.getLocalPart(), false),
-  NOTIFICATION(eu.de4a.iem.core.jaxb.dt.ObjectFactory._EventNotification_QNAME.getLocalPart(), false);
+public enum EMessageServiceTypes
+{
+  IM (eu.de4a.iem.core.jaxb.dr.ObjectFactory._RequestTransferEvidenceIM_QNAME.getLocalPart (), true),
+  USI (eu.de4a.iem.core.jaxb.dr.ObjectFactory._RequestTransferEvidenceUSI_QNAME.getLocalPart (), true),
+  LU (eu.de4a.iem.core.jaxb.dr.ObjectFactory._RequestTransferEvidenceLU_QNAME.getLocalPart (), true),
+  SN (eu.de4a.iem.core.jaxb.dr.ObjectFactory._RequestEventSubscription_QNAME.getLocalPart (), true),
+  RESPONSE (eu.de4a.iem.core.jaxb.dt.ObjectFactory._ResponseTransferEvidence_QNAME.getLocalPart (), false),
+  SUBSCRIPTION_RESP (eu.de4a.iem.core.jaxb.dt.ObjectFactory._ResponseEventSubscription_QNAME.getLocalPart (), false),
+  REDIRECT (eu.de4a.iem.core.jaxb.dt.ObjectFactory._USIRedirectUser_QNAME.getLocalPart (), false),
+  NOTIFICATION (eu.de4a.iem.core.jaxb.dt.ObjectFactory._EventNotification_QNAME.getLocalPart (), false);
 
-
-  private static final Map<String, EMessageServiceTypes> LOOKUP = new HashMap<>();
-  static {
-    for (final EMessageServiceTypes item : values())
-      LOOKUP.put(item.getType(), item);
+  private static final Map <String, EMessageServiceTypes> LOOKUP = new HashMap <> ();
+  static
+  {
+    for (final EMessageServiceTypes item : values ())
+      LOOKUP.put (item.getType (), item);
   }
 
   private final String type;
   private final boolean isRequest;
 
-  EMessageServiceTypes(@Nonnull @Nonempty final String type, final boolean isRequest) {
+  EMessageServiceTypes (@Nonnull @Nonempty final String type, final boolean isRequest)
+  {
     this.type = type;
     this.isRequest = isRequest;
   }
 
   @Nonnull
   @Nonempty
-  public String getType() {
+  public String getType ()
+  {
     return type;
   }
 
-  public boolean isRequest() {
+  public boolean isRequest ()
+  {
     return isRequest;
   }
 
   @Nonnull
   @Nonempty
-  public String getEndpointType() {
-    return name().toLowerCase(Locale.ROOT);
+  public String getEndpointType ()
+  {
+    return name ().toLowerCase (Locale.ROOT);
   }
 
   @Nullable
-  public static EMessageServiceTypes getByTypeOrNull(@Nullable final String type) {
-    return LOOKUP.get(type);
+  public static EMessageServiceTypes getByTypeOrNull (@Nullable final String type)
+  {
+    return LOOKUP.get (type);
   }
 }
--- de4a-connector/src/main/java/eu/de4a/connector/api/service/model/MessageExchangeWrapper.java
+++ de4a-connector/src/main/java/eu/de4a/connector/api/service/model/MessageExchangeWrapper.java
@@ -1,20 +1,25 @@
 package eu.de4a.connector.api.service.model;
 
 import javax.annotation.Nullable;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
+
 import com.helger.dcng.api.me.model.MEMessage;
 
-public class MessageExchangeWrapper extends ContextRefreshedEvent {
+public class MessageExchangeWrapper extends ContextRefreshedEvent
+{
   private transient MEMessage meMessage;
 
-  public MessageExchangeWrapper(final ApplicationContext source) {
-        super(source);
-        this.meMessage = null;
-    }
+  public MessageExchangeWrapper (final ApplicationContext source)
+  {
+    super (source);
+    this.meMessage = null;
+  }
 
   @Nullable
-  public MEMessage getMeMessage (){
+  public MEMessage getMeMessage ()
+  {
     return meMessage;
   }
 
