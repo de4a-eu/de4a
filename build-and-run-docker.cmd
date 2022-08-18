6c6bcb78f9a2d28c0e5e6e1e0c62c3906c836066
Philip Helger <philip@helger.com>
2022-08-18 19:19:40 +0200

    Helper file

---
+++ build-and-run-docker.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-11.0.16.8-hotspot
+call mvn clean install
+if errorlevel 1 goto error
+
+cd de4a-connector
+docker build --pull -t de4a/connector:iteration2 .
+if errorlevel 1 goto error
+
+docker run -d -p 8080:8080 --name de4a_conncector_it2 de4a/connector:iteration2
+if errorlevel 1 goto error
+
+goto end
+
+:error
+pause
+
+:end
